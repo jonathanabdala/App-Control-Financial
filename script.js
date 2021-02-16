163dae14c61839089f3f01154a6584b75131b155
jonathan <jonathanabdala@gmail.com>
2021-02-16 01:17:52 -0300

    Aula 03 JavaScript
    Implementação JavaScript

--- script.js
+++ script.js
@@ -1,79 +1,39 @@
-// Objetos JS, com duas funções que tem funcionalidades
+// Objetos JavaScript, com duas funções que tem funcionalidades
 const Modal = {
     open(){
         // Abrir modal
         // Adicionar a class active ao modal
        document.querySelector('.modal-overlay')
-       .classList.add('active')
+       .classList.
+       add('active')
     },
     close(){
         // fechar o Modal
         // remover a class active do modal
         document.querySelector('.modal-overlay')
-       .classList.remove('active')
+       .classList
+       .remove('active')
+    },
+}
+// Local para guardar 
+const Storage = {
+    // Pegar as informações
+    get () {
+        // O parse transforma string para array ou devolve este array vazio neste get
+        return JSON.parse(localStorage.getItem('dev.finances:transactions')) || []
+    },
+    // Guardar as informações 
+    set(transactions) {
+        // Transformar um Array em uma String usando um objeto chamado JSON com a funcionalidade stringify
+        localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
     }
 }
-// Criação do Array com seus Objetos
-const transactions = [
- {
-   
-    description:'Luz',
-    amount:-50000,
-    date:'23/01/2021'
- },
- {
-   
-    description:'Website',
-    amount:500000,
-    date:'23/01/2021'
- },
- {
-    
-    description:'Internet',
-    amount:-20000,
-    date:'23/01/2021'
- },
- {
-    
-    description:'APP',
-    amount:20000,
-    date:'23/01/2021'
- },
-]
-
 
 //Etapa responsavel para efetuar os calculos matemáticos:
 /*Preciso somar as entradas. Depois eu preciso somar as saídas e remover as entradas os valores das saídas */
 const Transaction = {
-    all : [
-        {
-          
-           description:'Luz',
-           amount:-50000,
-           date:'23/01/2021'
-        },
-        {
-          
-           description:'Website',
-           amount:500000,
-           date:'23/01/2021'
-        },
-        {
-           
-           description:'Internet',
-           amount:-20000,
-           date:'23/01/2021'
-        },
-        {
-           
-           description:'APP',
-           amount:20000,
-           date:'23/01/2021'
-        },
-       ]
-    // Refatoração
-    // Crinado um atalho utilizando 'all' para transactions dentro de Transaction
-    all: transactions,
+    all : Storage.get(),
+    
        // Funcionalidade de Adcionar transações com o 'push' a uma lista 
     add(transaction){
           Transaction.all.push(transaction)
@@ -112,7 +72,7 @@ const Transaction = {
         // Verificar se a transação é menor que zero;
         if(transaction.amount < 0){
         // Se for maior que zero, somar a uma variavel e retornar a variavel
-        expanses += transaction.amount;
+        expanse += transaction.amount;
         }
         })
         return expanses;
@@ -130,12 +90,13 @@ const DOM = {
 
     addTransaction(transaction, index) {
             const tr = document.createElement('tr') 
-            tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+            tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+            tr.dataset.index = index
             
             DOM.transactionsContainer.appendChild(tr)
      },
     
-    innerHTMLTransaction(transaction)  {
+    innerHTMLTransaction(transaction, index)  {
         // Parte lógica - usando ternário, verificando se um dado é verdadeiro ou falso
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
         // Receber a formatação
@@ -146,7 +107,8 @@ const DOM = {
            <td class="${CSSclass}">${transaction.amount}</td>
            <td class="date">${transaction.date}</td>
                 <td>
-                    <img src="./assets/minus.svg" alt="Remover transação">
+                // 
+                    <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
                 </td> 
                 `
 
@@ -170,23 +132,22 @@ const DOM = {
     },
 }
 
-// Criação dos formulários
-const Form = {
-    submit(event){
-        // Função para não realizar o comportamento padrão de enviar o formulário com as informações na URL
-        event.preventDefault()
-
-        // Verificar se todas as informações foram preenchidas
-        // Formatar os dados para salvar
-        // Salvar
-        // Apagar os dados do formulário
-        // Modal vai fechar
-        // Atualizar a aplicação
-    }
-}
-
 // Etapa 'Util' responsavel pela formação do moeda 
 const Utils = {
+    formatAmount(value){
+       
+        value = value * 100
+        //  Math.round arredonda o numero
+        return Math.round(value) 
+    },
+
+    formateDate(date){
+        // Formatando a separação de ano, mês e dia  por "-" o Array utilizando o splittedDate
+        const splittedDate = date.split("-")
+        // Separando as strings utilizando o Template literals (Template strings) 
+        return`${splittedDate[2]}/${splittedDate[1]}${splittedDate[0]}`
+
+    },
     // Formatação da moeda 
     formatCurrency(value){
         // Parte lógica - o valor da string sera um numero negativo ou não recebe nada.
@@ -205,15 +166,79 @@ const Utils = {
     return signal + value
 },
 
-}
-// Refatoranção para realizar o fluxo da aplicação
-// Funciinalidade que vai realizar a releitura das coisas
-cont App = {
-    init(){
+// Criação dos formulários
 
+const Form = {
+    // Fazendo um link entre JavaScript e HTML. Pegar o elemento inteiro do HTML e guardar no Form, para a validação.
+    description:document.querySelector('input#description'),
+    amount:document.querySelector('input#amount'),
+    date:document.querySelector('input#date'),
+
+    // Pegar cada  valor e deixar guardado na funtção getValues.
+    getValues(){
+        // Retornar um objeto que contenha:
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
     },
-    reload(){
+    validateFields() {
+        // Obter todos os valores:
+        const { description, amount, date } = Form.getValues()
 
+        // Lógica que vrifica se description, amount ou date estão vazios ou não
+        if( description.trim()=== " " ||
+            amount.trim()===" " ||
+            date.trim()===""){
+                throw new Error("Por favor, preencha todos os campos")
+            }
+    },
+
+    formatValues(){
+        let {description, amount, date} = Form.getValues()
+        
+        amount = Utils.formatAmount(amount)
+
+        date =  Utils.formateDate(date)
+
+        return {
+            description,
+            amount,
+            date
+        }
+    },
+
+    clearFields(){
+            // Limpando os campos
+            Form.description.value = ""
+            Form.amount.value = ""
+            Form.date.value = ""
+    }  
+ },
+submit(event){
+        // Função para não realizar o comportamento padrão de enviar o formulário com as informações na URL
+        event.preventDefault()
+
+        // Tratamento de Error com try(tentar)
+        try{
+            // Verificar se os campos estão validos
+           Form.validateFields()
+           const transaction =  Form.formatValues()
+           
+           // Adcionar uma transação
+          Transaction.add(transaction)
+          
+          // Apagar os dados do formulário
+           Form.clearFields()
+
+           // Modal vai fechar
+           Modal.close()
+           
+        } catch(error){
+            alert(error.message)
+        }
+        
     },
 }
 
@@ -222,16 +247,16 @@ const App = {
     init(){
 
 // Etapa das execuções da aplicação:
-Transaction.all.forcEach(transaction => {
-    DOM.addTransaction(transaction)
-    // Adiciona as transações que já existem na tela
-})
-
+Transaction.all.forcEach(DOM.addTransaction)
+// Adcionando na DOM
 
 DOM.updateBalance()
+// Atualizando o localStorage
 
+Storage.set(Transaction.all)
 
   },
+
   reload(){
     //   Ao entrar na DOM sera limpo
     DOM.clearTransactions()  
